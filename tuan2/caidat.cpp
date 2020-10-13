381dcc05a7699738a4d77a995daee487ba2ec7f4
vietn <nguyenhoangbaoviet1999@gmail.com>
2020-10-13 11:29:39 +0700

    da lam bai 2

--- tuan2/caidat.cpp
+++ tuan2/caidat.cpp
@@ -1,8 +1,24 @@
 #include "thuvien.h";
 void nhap(int &a, int &b)
 {
-	cout << "\n nhap vao a : ";
+	cout<< "------------DT VA CV HCN----------------";
+	cout << "\n nhap a";
 	cin >> a;
-	cout << "\n nhap vao b : ";
+	cout << "\n nhap b";
 	cin >> b;
+	double length = a;
+	double width = b;
+	cout << "\n dien tich hcn" << length * width;
+	cout << "\n chu vi hcn " << (length + width) * 2;
+	cout << "\n ------------DT va CV HV----------------";
+	cout << "\n nhap vao 2 canh a";
+	cin >> a;
+	cout << "\n DT HV" << a*a;
+	cout << "\n CV HV" << a * 4;
+	cout << "\n ------------DT va CV HT----------------";
+	float r;
+	cout << "\n nhap ban kinh r=";
+	cin >> r;
+	cout << "\n DT HT" << r * r*3.14;
+	cout << "\n CV HT" << 2 * 3.14*r;
 }
--- tuan2/thuvien.h
+++ tuan2/thuvien.h
@@ -1,3 +1,4 @@
 #include <iostream>;
+#include <math.h>;
 using namespace std;
 void nhap(int &a, int &b);
